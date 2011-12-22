116c8d82bd35bd204a8ad6384aa236d3dcc23052
Patrick Drechsler <patrick@pdrechsler.de>
2011-12-22 02:39:13 +0100

    initial commit

---
+++ _01_create_initial_submodules.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git submodule add http://github.com/nex3/haml.git
+git submodule add git://repo.or.cz/org-mode.git
+git submodule add http://github.com/pezra/rspec-mode.git
+git submodule add git://github.com/nex3/ert.git
+git submodule add git://github.com/eschulte/jump.el.git
+git submodule add git://github.com/eschulte/rinari.git
+git submodule add https://github.com/rejeep/ruby-end.git
---
+++ _02_get_rinari_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd rinari
+git submodule init
+git submodule update
