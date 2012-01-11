134d4b644a04a7fe49a0a943d160e7f11fc1ac6b
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-01-11 22:50:18 +0100

    submodule stuff...

---
+++ _02_submodule_updating_after_initial_clone.sh
@@ -0,0 +1,8 @@
+
+## see http://chrisjean.com/2009/04/20/git-submodules-adding-using-removing-and-updating/
+
+## You might have to repeat these commands in every submodule in case these submodules include other submodules... ;-(
+
+git submodule init
+git submodule update
+
